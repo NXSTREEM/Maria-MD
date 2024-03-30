74724c4c5d6c9e7864e62d7f215f616846cf088d
NXSTREEM <165490567+NXSTREEM@users.noreply.github.com>
2024-03-30 16:57:35 +0200

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,18 +2,18 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = ['+27 76 248 5717']
+global.ownername = "leago"//owner name
+global.ytname = "YT: Preston.inc"
+global.socialm = "GitHub: NXSTREEM"
+global.location = "south Africa"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'Nigger-𝐌𝐃' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
-global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.stickername = '💙𓆩‎✘sus✘𓆪_✰'
+global.packname = 'Sticker By NXSTREEM'
+global.author = 'nigger Bot'
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
